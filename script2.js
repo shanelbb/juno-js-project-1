12b7c22dfcbc80b80acf0b8fab9f4a6448ca314d
Shanel Beebe <shanelbeebe@icloud.com>
2019-12-06 10:12:59 -0500

    wip

--- script2.js
+++ script2.js
@@ -196,300 +196,312 @@
 //   }
 // ];
 
-const quiz = [
-  {
-    name: "Question 1",
-    question:
-      "In Harry's first lesson with Snape, what is the first potion the class learns to make?",
-    answers: [
+$(function() {
+  const quizData = {
+    quiz: [
       {
-        value: "A potion to cure boils"
+        name: "Question 1",
+        question:
+          "In Harry's first lesson with Snape, what is the first potion the class learns to make?",
+        answers: [
+          {
+            value: "A potion to cure boils"
+          },
+          {
+            value: "Confusing Concoction"
+          },
+          {
+            value: "Polyjuice Potion"
+          },
+          {
+            value: "An antidote for common poisons"
+          }
+        ],
+        correctAnswer: 0
       },
       {
-        value: "Confusing Concoction"
+        name: "Question 2",
+        question:
+          "What would you get if you added powdered root of asphodel to an infusion of wormwood?",
+        answers: [
+          {
+            value: "Pepperup Potion"
+          },
+          {
+            value: "Wolfsbane Potion"
+          },
+          {
+            value: "Draught of Living Death"
+          },
+          {
+            value: "Draught of Peace"
+          }
+        ],
+        correctAnswer: 2
       },
       {
-        value: "Polyjuice Potion"
+        name: "Question 3",
+        question:
+          "What is the incantation of the spell that can expel a boggart?",
+        answers: [
+          {
+            value: "Waddiwasi"
+          },
+          {
+            value: "Riddikulus"
+          },
+          {
+            value: "Dissendium"
+          },
+          {
+            value: "Morsmordre"
+          }
+        ],
+        correctAnswer: 1
       },
       {
-        value: "An antidote for common poisons"
+        name: "Question 4",
+        question: "What does Neville Longbottom use to kill Nagini?",
+        answers: [
+          {
+            value: "A knife"
+          },
+          {
+            value: "Basilisk Fang"
+          },
+          {
+            value: "His wand"
+          },
+          {
+            value: "The sword of Gryffindor"
+          }
+        ],
+        correctAnswer: 3
+      },
+      {
+        name: "Question 5",
+        question: "What was the first name of Sirius Black's mother?",
+        answers: [
+          {
+            value: "Willhemina"
+          },
+          {
+            value: "Walburga"
+          },
+          {
+            value: "Winnifred"
+          },
+          {
+            value: "Warmina"
+          }
+        ],
+        correctAnswer: 1
+      },
+      {
+        name: "Question 6",
+        question:
+          "What spell do Death Eaters use to conjure up the Dark Mark in the sky?",
+        answers: [
+          {
+            value: "Morsmordre"
+          },
+          {
+            value: "Piertotum Locomotor"
+          },
+          {
+            value: "Stupefy"
+          },
+          {
+            value: "Avada Kedavra"
+          }
+        ],
+        correctAnswer: 0
+      },
+      {
+        name: "Question 7",
+        question: "What is Albus Dumblesdore's full name?",
+        answers: [
+          {
+            value: "Albus Peter Weatherby Barnabus Dumbledore"
+          },
+          {
+            value: "Albus Polonius Wilfred Bertram Dumbledore"
+          },
+          {
+            value: "Albus Percival Wulfric Brian Dumbledore"
+          },
+          {
+            value: "Albus Pericles Wolfgang Bernard Dumbledore"
+          }
+        ],
+        correctAnswer: 2
+      },
+      {
+        name: "Question 8",
+        question: "What form does Hermione's Patronus take?",
+        answers: [
+          {
+            value: "Dove"
+          },
+          {
+            value: "Jack Russell Terrier"
+          },
+          {
+            value: "Otter"
+          },
+          {
+            value: "Hare"
+          }
+        ],
+        correctAnswer: 2
+      },
+      {
+        name: "Question 9",
+        question: "What materials was Harry's wand made out of?",
+        answers: [
+          {
+            value: "Hawthorn and unicorn hair"
+          },
+          {
+            value: "Holly and phoenix feather"
+          },
+          {
+            value: "Vine and dragon heartstring"
+          },
+          {
+            value: "Willow and unicorn hair"
+          }
+        ],
+        correctAnswer: 1
+      },
+      {
+        name: "Question 10",
+        question: "Which family did Winky the house elf serve?",
+        answers: [
+          {
+            value: "Malfoys"
+          },
+          {
+            value: "Diggorys"
+          },
+          {
+            value: "Potters"
+          },
+          {
+            value: "Crouches"
+          }
+        ],
+        correctAnswer: 3
       }
-    ],
-    correctAnswer: 0
-  },
-  {
-    name: "Question 2",
-    question:
-      "What would you get if you added powdered root of asphodel to an infusion of wormwood?",
-    answers: [
-      {
-        value: "Pepperup Potion"
-      },
-      {
-        value: "Wolfsbane Potion"
-      },
-      {
-        value: "Draught of Living Death"
-      },
-      {
-        value: "Draught of Peace"
-      }
-    ],
-    correctAnswer: 2
-  },
-  {
-    name: "Question 3",
-    question: "What is the incantation of the spell that can expel a boggart?",
-    answers: [
-      {
-        value: "Waddiwasi"
-      },
-      {
-        value: "Riddikulus"
-      },
-      {
-        value: "Dissendium"
-      },
-      {
-        value: "Morsmordre"
-      }
-    ],
-    correctAnswer: 1
-  },
-  {
-    name: "Question 4",
-    question: "What does Neville Longbottom use to kill Nagini?",
-    answers: [
-      {
-        value: "A knife"
-      },
-      {
-        value: "Basilisk Fang"
-      },
-      {
-        value: "His wand"
-      },
-      {
-        value: "The sword of Gryffindor"
-      }
-    ],
-    correctAnswer: 3
-  },
-  {
-    name: "Question 5",
-    question: "What was the first name of Sirius Black's mother?",
-    answers: [
-      {
-        value: "Willhemina"
-      },
-      {
-        value: "Walburga"
-      },
-      {
-        value: "Winnifred"
-      },
-      {
-        value: "Warmina"
-      }
-    ],
-    correctAnswer: 1
-  },
-  {
-    name: "Question 6",
-    question:
-      "What spell do Death Eaters use to conjure up the Dark Mark in the sky?",
-    answers: [
-      {
-        value: "Morsmordre"
-      },
-      {
-        value: "Piertotum Locomotor"
-      },
-      {
-        value: "Stupefy"
-      },
-      {
-        value: "Avada Kedavra"
-      }
-    ],
-    correctAnswer: 0
-  },
-  {
-    name: "Question 7",
-    question: "What is Albus Dumblesdore's full name?",
-    answers: [
-      {
-        value: "Albus Peter Weatherby Barnabus Dumbledore"
-      },
-      {
-        value: "Albus Polonius Wilfred Bertram Dumbledore"
-      },
-      {
-        value: "Albus Percival Wulfric Brian Dumbledore"
-      },
-      {
-        value: "Albus Pericles Wolfgang Bernard Dumbledore"
-      }
-    ],
-    correctAnswer: 2
-  },
-  {
-    name: "Question 8",
-    question: "What form does Hermione's Patronus take?",
-    answers: [
-      {
-        value: "Dove"
-      },
-      {
-        value: "Jack Russell Terrier"
-      },
-      {
-        value: "Otter"
-      },
-      {
-        value: "Hare"
-      }
-    ],
-    correctAnswer: 2
-  },
-  {
-    name: "Question 9",
-    question: "What materials was Harry's wand made out of?",
-    answers: [
-      {
-        value: "Hawthorn and unicorn hair"
-      },
-      {
-        value: "Holly and phoenix feather"
-      },
-      {
-        value: "Vine and dragon heartstring"
-      },
-      {
-        value: "Willow and unicorn hair"
-      }
-    ],
-    correctAnswer: 1
-  },
-  {
-    name: "Question 10",
-    question: "Which family did Winky the house elf serve?",
-    answers: [
-      {
-        value: "Malfoys"
-      },
-      {
-        value: "Diggorys"
-      },
-      {
-        value: "Potters"
-      },
-      {
-        value: "Crouches"
-      }
-    ],
-    correctAnswer: 3
-  }
-];
+    ]
+  };
 
-// Cached Selectors
-const $quiz = $("#quiz");
-const $welcome = $("#welcome");
-const $start = $("#start");
-const $question = $("#question");
-const $answer = $("#answer");
-// const $controls = $("#controls");
-const $form = $("form");
-const $submit = $("#submit");
-const $back = $("#back");
-const $next = $("#next");
-const $results = $("#results");
+  // Cached Selectors
+  const $quiz = $("#quiz");
+  const $welcome = $("#welcome");
+  const $start = $("#start");
+  const $question = $("#question");
+  const $answer = $("#answer");
+  const $form = $("form");
+  const $submit = $("#submit");
+  const $back = $("#back");
+  const $next = $("#next");
+  const $results = $("#results");
 
-let shuffledQuestions;
+  // let shuffledQuestions;
 
-$question.addClass("hide");
-$answer.addClass("hide");
-// $controls.addClass("hide");
-$submit.addClass("hide");
-$results.addClass("hide");
+  $question.addClass("hide");
+  $answer.addClass("hide");
+  $submit.addClass("hide");
+  $results.addClass("hide");
+  $(".score-board").addClass("hide");
 
-const createForm = function () {
-  quiz.forEach(function (question) {
-    const htmlToAppend = `
-      <fieldset>
-        <legend>${question.question}</legend>
-        <label for="answer1">${question.answers[0].value}</label>
-        <input value="0" checked type="radio" name=${question.name} id="answer1"/>
-        <label for="answer2">${question.answers[1].value}</label>
-        <input  value="1" type="radio" name=${question.name} id="answer1"/>
-        <label for="answer3">${question.answers[2].value}</label>
-        <input  value="2" type="radio" name=${question.name} id="answer1"/>
-        <label for="answer4">${question.answers[3].value}</label>
-        <input  value="3" type="radio" name=${question.name} id="answer1"/>
-      </fieldset>
-    `
-    $(htmlToAppend).insertBefore("#submit");
-  })
-}​
-
-$start.on("click", () => {
-  quiz.forEach(function(index) {
-    shuffledQuestions = quiz.sort(() => Math.random() - 0.5);
-    ​
-    createForm();
+  $start.on("click", () => {
+    // quiz.forEach(function(index) {
+    //   shuffledQuestions = quiz.sort(() => Math.random() - 0.5);
+    // createForm();
 
     $question.removeClass("hide");
     $answer.removeClass("hide");
     $next.removeClass("hide");
-    $controls.removeClass("hide");
     $submit.removeClass("hide");
 
     $welcome.addClass("hide");
     $back.addClass("hide");
+    // });
   });
-});
 
-
-  // array of correct answers
-  let correctArray = quiz.map(function (question) {
+  let correctArray = quizData.quiz.map(function(question) {
     return question.correct;
-  })
-​
-  // Function to check if the values in the submitted answer are correct or not
-  const checkValues = function (array) {
-    let correctArray = [];
-    let incorrectArray = [];
-    let score = 0;
-    array.forEach(function (answer, i) {
-      answerNumber = parseInt(answer)
-      if (answerNumber === quiz[i].correct) {
-        correctArray.push(answer);
-      } else {
-        incorrectArray.push(answer)
-      }
-    })
-    score = score + correctArray.length * 10;
-    showScore(score);
-  }
-​
-  const formSubmit = function () {
-    // let score = 0;
-    $("form").on("submit", function (e) {
+  });
+
+  const formSubmit = function(i) {
+    $("form").on("click", "button", function(e) {
       e.preventDefault();
-      console.log("submitted")
-      // Store all the values of the selected questions in an "array-like-object"
-      const answers = $("input[type='radio']:checked").map(function () {
-        return $(this).val();
-      })
-      // the answers are stored in a jQuery object so we make an array to loop through
-      const answersArray = Array.from(answers);
-      // call the function that submits all the answers
-      checkValues(answersArray);
-    })
-  }
-​
-  const showScore = function (score) {
-    $(".score-board").append(`<p>Score: ${score} </p>`)
-  }
-  formSubmit();
-})
+      const userChoice = $(this)
+        .prev()
+        .children("input[type='radio']:checked")
+        .val();
+      const userChoiceNumber = parseInt(userChoice);
+      const fieldSetID = $(this)
+        .prev()
+        .attr("id");
+      const correctAnswer = quizData.quiz[fieldSetID].correct;
+      if (userChoiceNumber === correctAnswer) {
+        quizData.score++;
+        showScore();
+      } else {
+        quizData.score--;
+        showScore();
+      }
+      $(this).hide();
+      $(this)
+        .prev()
+        .hide();
+      showNextQuestion(parseInt(fieldSetID));
+    });
+  };
+  const showNextQuestion = index => {
+    if (index === quizData.quiz.length - 1) {
+      showFinalScreen();
+    } else {
+      showQuestion(index + 1);
+    }
+  };
+  const showScore = function(score) {
+    console.log(quizData.score);
+    $(".score-board").html(`<h4>Score: ${quizData.score * 10} </h4>`);
+  };
+  const showQuestion = function(i) {
+    const htmlToAppend = `
+      <fieldset id=${i}>
+        <legend><h4>${quizData.quiz[i].question}</h4></legend>
+        <label for="answer1">${quizData.quiz[i].answers[0].value}</label>
+        <input value="0" checked type="radio" name=${quizData.quiz[i].name} id="answer1"/>
+        <label for="answer2">${quizData.quiz[i].answers[1].value}</label>
+        <input  value="1" type="radio" name=${quizData.quiz[i].name} id="answer1"/>
+        <label for="answer3">${quizData.quiz[i].answers[2].value}</label>
+        <input  value="2" type="radio" name=${quizData.quiz[i].name} id="answer1"/>
+        <label for="answer4">${quizData.quiz[i].answers[3].value}</label>
+        <input  value="3" type="radio" name=${quizData.quiz[i].name} id="answer1"/>
+      </fieldset>
+      <button id="next">next</button>
+    `;
+    $(".content").append(htmlToAppend);
+  };
+  const showFinalScreen = () => {
+    const htmlToAppend = `
+  <h4>This is the final screen</h4>
+  `;
+
+    $(".score-board").removeClass("hide");
+    $(".content").html(htmlToAppend);
+  };
+  const init = function() {
+    showQuestion(0);
+    formSubmit();
+    showScore();
+  };
+
+  init();
+});
